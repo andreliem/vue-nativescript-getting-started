1138bd05b22353fec2344ae4b5208a485eac1ac3
Andre Liem <andre.liem@gmail.com>
2018-06-11 15:02:51 -0700

    finished tutorial

---
+++ src/components/Sample.vue
@@ -0,0 +1,44 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Sample Page">
+      <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$router.push('/home')"/>
+    </ActionBar>
+
+    <StackLayout class="sample">
+      <Label class="body" textWrap=true text="Date Pick"/>
+      <DatePicker @loaded="onDatePickerLoaded" @dateChange="onDateChanged" />
+      <Button text="Button" @tap="buttonTap" />
+    </StackLayout>
+  </Page>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        text: "Form Examples"
+      };
+    },
+    methods: {
+      onDateChanged () {
+        alert({
+          title: "Date Changed",
+          message: "The date was changed",
+          okButtonText: "OK"
+        }).then(() => {
+          console.log("closed it");
+        });
+      },
+      buttonTap () {
+        action("Take Action", "Cancel", ["Option1", "Option2"])
+          .then(result => {
+            console.log(result);
+          });
+      }
+    }
+  };
+</script>
+<style scoped>
+  .sample {
+    margin: 20;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter);
 import Home from '../components/Home';
 import HelloWorld from '../components/HelloWorld';
 import Counter from '../components/Counter';
+import Sample from '../components/Sample';
 
 const router = new VueRouter({
   pageRouting: true,
@@ -29,6 +30,13 @@ const router = new VueRouter({
       component: Counter,
       meta: {
         title: 'Counter',
+      },
+    },
+    {
+      path: '/sample',
+      component: Sample,
+      meta: {
+        title: 'Sample',
       },
     },
     {path: '*', redirect: '/home'},
